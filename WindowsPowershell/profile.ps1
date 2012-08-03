96d90c62dffe3d87362e1d9a96e658227190b7ab
Daniel Kiessling <dkiessling@googlemail.com>
2012-08-03 11:44:14 +0200

    added item color function

--- WindowsPowershell/profile.ps1
+++ WindowsPowershell/profile.ps1
@@ -21,6 +21,50 @@ set-alias ll Get-ChildItem
 set-alias fortune "$powershellPath/fortune.ps1"
 fortune
 
+remove-item alias:ls
+set-alias ls Get-ChildItemColor
+
+function Get-ChildItemColor {
+    $fore = $Host.UI.RawUI.ForegroundColor
+
+    Invoke-Expression ("Get-ChildItem $args") |
+    %{
+        if ($_.GetType().Name -eq 'DirectoryInfo') {
+            $Host.UI.RawUI.ForegroundColor = 'White'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        } elseif ($_.Name -match '\.(zip|tar|gz|rar)$') {
+            $Host.UI.RawUI.ForegroundColor = 'Blue'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        } elseif ($_.Name -match '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$') {
+            $Host.UI.RawUI.ForegroundColor = 'Green'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        } elseif ($_.Name -match '\.(txt|cfg|conf|ini|csv|sql|xml|config)$') {
+            $Host.UI.RawUI.ForegroundColor = 'Cyan'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        } elseif ($_.Name -match '\.(cs|asax|aspx.cs)$') {
+            $Host.UI.RawUI.ForegroundColor = 'Yellow'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        } elseif ($_.Name -match '\.(aspx|spark|master)$') {
+            $Host.UI.RawUI.ForegroundColor = 'DarkYellow'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        } elseif ($_.Name -match '\.(sln|csproj)$') {
+            $Host.UI.RawUI.ForegroundColor = 'Magenta'
+            echo $_
+            $Host.UI.RawUI.ForegroundColor = $fore
+        }
+        else {
+            $Host.UI.RawUI.ForegroundColor = $fore
+            echo $_
+        }
+    }
+}
+
 function get-adminuser() {
    $id = [Security.Principal.WindowsIdentity]::GetCurrent()
    $p = New-Object Security.Principal.WindowsPrincipal($id)
